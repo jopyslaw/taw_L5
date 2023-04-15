d8bb6c5f5d834ec968320f74f524b5be6e0d1d4d
jopyslaw <jopyslaw@gmail.com>
2023-04-15 12:03:15 +0200

    Lab6 zadania

---
+++ app/app.js
@@ -0,0 +1,29 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import routes from "./REST/routes.js";
+import config from "./config.js";
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Instancja serwera
+const app = express();
+app.use(express.static(__dirname + "/public"));
+
+// middleware (funkcje) developerskie
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "2048kb" }));
+
+// cors - mechanizm umożliwiający współdzielenie zasobów pomiędzy serwerami znajdującymi się w różnych domenach
+app.use(cors());
+
+routes(app);
+
+// uruchomienie serwera
+app.listen(config.port, function () {
+  console.info(`Server is running at ${config.port}`);
+});
